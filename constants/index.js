b4fe95026dd0d7225313ed98b79402f204e756ef
Mário Baltazar <mario.baltazar@innowave.tech>
2023-02-07 15:41:15 +0000

    first commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xfC3BEf8eA5Ca6aaB067BEcDBb76E3104B29Dec19";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
